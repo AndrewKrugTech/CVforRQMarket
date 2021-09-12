7ae1c095c32d38b070f74528d47d81c1af884557
AndrewKrugTech <andreas.harlov@yandex.ru>
2021-09-12 12:33:13 +0500

    Initial commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pytesseract
 # pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 # Подключение фото
-img = cv2.imread('yourPhoto.png')
+img = cv2.imread('monitor-1.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # Будет выведен весь текст с картинки
