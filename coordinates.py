a2fc0df448cb08777a560a00f400aefa075935d2
AndrewKrugTech <andreas.harlov@yandex.ru>
2021-09-25 19:22:03 +0500

    Update coordinates.py

--- coordinates.py
+++ coordinates.py
@@ -1,6 +1,6 @@
 import pyautogui
 import keyboard as kb
-
+"""
 def defineURPoint():
     x1 = int()
     y1 = int()
@@ -24,4 +24,27 @@ def defineRefreshPoint():
         if kb.is_pressed("c"):
             x3, y3 = pyautogui.position()
     return x3, y3
+"""
 
+
+def definePoints():
+    x1 = int()
+    y1 = int()
+    x2 = int()
+    y2 = int()
+    x3 = int()
+    y3 = int()
+    while y3 == 0:
+        if kb.is_pressed("z"):
+            x1, y1 = pyautogui.position()
+            x1 += 1600
+            y1 -= 180
+        elif kb.is_pressed("x"):
+            x2, y2 = pyautogui.position()
+            x2 += 1600
+            y2 -= 180
+        elif kb.is_pressed("v"):
+            x3, y3 = pyautogui.position()
+            x3 += 1600
+            y3 -= 180
+    return x1, y1, x2, y2, x3, y3
